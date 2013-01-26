20b6dc174a8e355999080647d2298770e695e808
Kir Shatrov <shatrov@me.com>
2013-01-27 00:36:16 +0200

    Ignoring 404 exceptions

--- RatchetioErrorHandler.php
+++ RatchetioErrorHandler.php
@@ -5,7 +5,10 @@ class RatchetioErrorHandler extends CErrorHandler
 {
   protected function handleException($exception)
   {
-    Ratchetio::report_exception($exception);
+    $ignored = ($exception instanceof CHttpException && $exception->statusCode == 404);
+    if (!$ignored) {
+      Ratchetio::report_exception($exception);
+    }
     parent::handleException($exception);
   }
 }
