848baaee9935379df5c2d82d8273f56e035087da
sergebezborodov <mail@sergebezborodov.com>
2013-02-06 12:05:49 +0200

    convert spaces to tabs

--- RatchetioErrorHandler.php
+++ RatchetioErrorHandler.php
@@ -3,29 +3,29 @@ require_once 'ratchetio.php';
 
 class RatchetioErrorHandler extends CErrorHandler
 {
-    /**
-     * Send PHP Errors to Ratchetio
-     *
-     * @var bool
-     */
-    public $phpErrorsToRatchetio = false;
+	/**
+	 * Send PHP Errors to Ratchetio
+	 *
+	 * @var bool
+	 */
+	public $phpErrorsToRatchetio = false;
 
-    protected function handleException($exception)
-    {
-        $ignored = ($exception instanceof CHttpException && $exception->statusCode == 404);
-        if (!$ignored) {
-            Ratchetio::report_exception($exception);
-        }
-        parent::handleException($exception);
-    }
+	protected function handleException($exception)
+	{
+		$ignored = ($exception instanceof CHttpException && $exception->statusCode == 404);
+		if (!$ignored) {
+			Ratchetio::report_exception($exception);
+		}
+		parent::handleException($exception);
+	}
 
 
-    protected function handleError($event)
-    {
-        if ($this->phpErrorsToRatchetio) {
-            Ratchetio::report_php_error($event->code, $event->message, $event->file, $event->line);
-        }
+	protected function handleError($event)
+	{
+		if ($this->phpErrorsToRatchetio) {
+			Ratchetio::report_php_error($event->code, $event->message, $event->file, $event->line);
+		}
 
-        parent::handleError($event);
-    }
+		parent::handleError($event);
+	}
 }
